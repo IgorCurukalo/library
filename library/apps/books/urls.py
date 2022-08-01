d49a9cc01fbbed1abe066e0e6aee1629b7d1fee6
IgorCuruklo <igorcurukalo@gmail.com>
2022-08-01 21:02:06 +0500

    generyc

--- library/apps/books/urls.py
+++ library/apps/books/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from apps.books.views import PublishingHouseAPIView, PublishingHouseCreateAPIView, BookAPIView, BookCreateAPIView, AuthorAPIView, AuthorCreateAPIView
+from apps.books.views import PublishingHouseAction, PublishingHouseRetrieve
+    # PublishingHouseAPIView, PublishingHouseCreateAPIView, BookAPIView, BookCreateAPIView, AuthorAPIView, AuthorCreateAPIView
 
 
 urlpatterns = [
-    path('publishing_house/<int:pk>', PublishingHouseAPIView.as_view()),
-    path('publishing_house/', PublishingHouseCreateAPIView.as_view()),
-    path('book/<int:pk>', BookAPIView.as_view()),
-    path('book/', BookCreateAPIView.as_view()),
-    path('author/<int:pk>', AuthorAPIView.as_view()),
-    path('author/', AuthorCreateAPIView.as_view())
+    path('publishing_house/', PublishingHouseAction.as_view()),
+    # path('publishing_house_/', PublishingHouseRetrieve.as_view()),
+
+    
+    # path('publishing_house/', PublishingHouseCreateAPIView.as_view()),
+    # path('publishing_house/<int:pk>', PublishingHouseAPIView.as_view()),
+    # path('publishing_house/', PublishingHouseCreateAPIView.as_view()),
+    # path('book/<int:pk>', BookAPIView.as_view()),
+    # path('book/', BookCreateAPIView.as_view()),
+    # path('author/<int:pk>', AuthorAPIView.as_view()),
+    # path('author/', AuthorCreateAPIView.as_view())
 ]
