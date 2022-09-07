a7bb6c25696a6a69e7eb48bf7fd1a2bc17b72cb0
IgorCuruklo <igorcurukalo@gmail.com>
2022-09-07 20:48:38 +0500

    vewset

--- library/signals/pre_delete.py
+++ library/signals/pre_delete.py
@@ -1,6 +1,9 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-@receiver(pre_delete)
-def save_log():
-    pass
+from apps.books.models import Book
+
+@receiver(pre_delete, sender=Book)
+def save_delete_log(sender, instance, **kwargs):
+
+    print(f'User try to delete object with instance: {instance}')
